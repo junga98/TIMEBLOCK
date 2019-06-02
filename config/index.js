291bd13a9f0c3e85277cf242b71614c477688cfd
junga <www.719478441@qq.com>
2019-06-02 14:19:54 +0800

    Add files via upload

---
+++ config/index.js
@@ -0,0 +1,29 @@
+var path = require('path')
+
+module.exports = {
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    productionSourceMap: true,
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
+  dev: {
+    env: require('./dev.env'),
+    hosts: 'localhost',
+    // hosts: '172.16.101.78',
+    // port: 8080,
+    autoOpenBrowser: true,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {
+      "/api": "http://localhost:4300"
+      // "/api": "http://172.16.101.78:4300"
+    },
+    cssSourceMap: false
+  }
+}
